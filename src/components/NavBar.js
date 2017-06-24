a2d9622578543522af87dbe4874d1e553f4720b2
geekodour <hrishikeshbman@gmail.com>
2017-06-24 15:34:17 +0530

    added search box

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import MediaQuery from 'react-responsive'
 import logo from '../logo.svg'
 
+import AppBar from 'material-ui/AppBar'
+import Paper from 'material-ui/Paper'
+import TextField from 'material-ui/TextField'
+
+const style = {
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
 const NavBar = props => (
-  <nav className="nav" style={{backgroundColor:'#87c8f4'}}>
-    <div className="container">
-      <div className="nav-left">
-        <Link to="/" className="nav-item">
-          <img src={logo} alt="poop" className="logomark"/>
-        </Link>
+  <div className="">
+  <MediaQuery maxDeviceWidth={800}>
+    <AppBar
+    title="markmymind"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+    />
+  </MediaQuery>
+  <MediaQuery minDeviceWidth={1224}>
+    <nav className="nav" style={{backgroundColor:'#87c8f4'}}>
+      <div className="container">
+        <div className="nav-left">
+          <Link to="/" className="nav-item">
+            <img src={logo} alt="poop" className="logomark"/>
+          </Link>
+        </div>
+        <span className="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div className="nav-right nav-menu">
+          <a className="nav-item is-tab">Log out</a>
+        </div>
       </div>
-      <span className="nav-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
-      <div className="nav-right nav-menu">
-        <a className="nav-item is-tab">Log out</a>
+    </nav>
+    <nav className="nav" style={{backgroundColor:'#87c8f4'}}>
+      <div className="container">
+        <div className="nav-right">
+           <a className="nav-item">
+              <Paper style={style} zDepth={1}>
+                <TextField hintText="Hint Text" />
+		      </Paper>
+           </a>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </MediaQuery>
+  </div>
 )
 
 
